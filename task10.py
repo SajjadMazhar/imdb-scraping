bef8a93152461a0eb63fab9631d1c4aeb56f3336
SajjadMazhar <sajjadmazharjr@gmail.com>
2022-02-19 18:23:02 +0530

    pending task completed

---
+++ task10.py
@@ -0,0 +1,26 @@
+
+import json
+from pprint import pprint
+
+with open("task4.json", "r") as f:
+    object = json.load(f)
+
+directors = []
+for data in object:
+    for director in data['director']:
+        if director not in directors:
+            directors.append(director)
+
+languages_with_director = []
+for director in directors:
+    langs = {}
+    for data in object:
+        if director in data['director']:
+            for lang in data['language']:
+                if lang not in langs:
+                    langs[lang] = 1
+                else:
+                    langs[lang] += 1
+    languages_with_director.append({director:langs})
+
+pprint(languages_with_director)
